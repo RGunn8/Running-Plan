055f114e27056e8bce2804b3f927ae54d5887171
ryangunn <ryan.gunn@dialexa.com>
2023-01-30 16:08:24 -0600

    Initial Commit

---
+++ Running Plan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Running Plan
+//
+//  Created by Ryan Gunn on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()d
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Running Plan/Running_PlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Running_PlanApp.swift
+//  Running Plan
+//
+//  Created by Ryan Gunn on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Running_PlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
